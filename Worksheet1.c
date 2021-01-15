dc7d686ce2882c0ef444a7bc32c2f262c91a16df
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-01-15 15:22:44 -0500

    Finished worksheet 1

--- Worksheet1.c
+++ Worksheet1.c
@@ -1,9 +1,12 @@
+// defines
 #define MAX_LEN_NAME 20
 
+// invludes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//structs
 typedef struct justATag {
     char name[MAX_LEN_NAME];
     char* supervisor;
@@ -11,16 +14,55 @@ typedef struct justATag {
     float currentPercentage;
 } gradStudent;
 
-void oneMoreDone(gradStudent student) {
-    student.numCoursesDone += 1;
+// prototypes
+void oneMoreDone(gradStudent*);
+void swapSupervisors(gradStudent*, gradStudent*);
+
+// functions
+void oneMoreDone(gradStudent* student) {
+    float grade;
+    float newPercentage;
+
+    printf("\nPlease input your grade for the course (max of 100%%): ");
+    scanf("%f", &grade);
+    while (grade > 100 || grade < 0) {
+        printf("Error, invalid grade, please try again: ");
+        scanf("%f", &grade);
+    }
+
+    // newPercentage = student->currentPercentage * ((float)student->numCoursesDone / (student->numCoursesDone + 1));
+    newPercentage = ((((float)(student->numCoursesDone - 1) / student->numCoursesDone) * student->currentPercentage) + ((1 - ((float)(student->numCoursesDone - 1) / student->numCoursesDone)) * grade));
+    student->currentPercentage = newPercentage;
+    student->numCoursesDone += 1;
 }
 
+void swapSupervisors(gradStudent* student1, gradStudent* student2) {
+    char* temp = student1->supervisor;
+
+    student1->supervisor = student2->supervisor;
+    student2->supervisor = temp;
+}
+
+// main
 int main(void) {
     gradStudent g1;
-    strcpy(g1.name, "max");
-    printf("%s", g1.name);
+    gradStudent g2;
+    strcpy(g1.name, "Harry Dang");
+
+    g1.supervisor = malloc(sizeof(char) * MAX_LEN_NAME);
+    strcpy(g1.supervisor, "Ria Jonas");
     g1.numCoursesDone = 3;
-    g1.currentPercentage = 87;
+    g1.currentPercentage = 87.0;
+
+    printf("%f\n", g1.currentPercentage);
+    oneMoreDone(&g1);
+    printf("%f\n", g1.currentPercentage);
+
+    g2.supervisor = malloc(sizeof(char) * MAX_LEN_NAME);
+    strcpy(g2.supervisor, "Dough Jonas");
+    printf("%s\n%s\n", g1.supervisor, g2.supervisor);
+    swapSupervisors(&g1, &g2);
+    printf("%s\n%s\n", g1.supervisor, g2.supervisor);
 
     return 0;
 }
