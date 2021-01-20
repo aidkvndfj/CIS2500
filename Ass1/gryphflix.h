bd84569a02f8a65d676f645d3a9910798371be50
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-01-20 00:28:27 -0500

    setup basic files for Ass1

---
+++ Ass1/gryphflix.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define NUMBER_REVIEWERS 10
+#define NUMBER_MOVIES 3
+#define MAX_STR 50
+#define MAX_WORDS 10
+
+typedef struct reviewStruct {
+    char reviewer[MAX_STR];
+    int feedback[NUMBER_MOVIES];
+} review;
+
+FILE *openFileForReading(char *fileName);
+int readMovies(FILE *file, char movieNames[NUMBER_MOVIES][MAX_STR]);
+int readReviews(FILE *file, struct reviewStruct reviews[NUMBER_REVIEWERS]);
+int getMostCriticalReviewers(struct reviewStruct reviews[NUMBER_REVIEWERS],
+                             char mostCriticalReviewers[NUMBER_REVIEWERS][MAX_STR]);
+int getMostRecommendedMovies(const char movies[NUMBER_MOVIES][MAX_STR],
+                             const struct reviewStruct reviews[NUMBER_REVIEWERS],
+                             char mostRecommendedMovies[NUMBER_REVIEWERS][MAX_STR]);
+int predictMovie(const char movie[MAX_STR]);
+
+//Additional
+int enumerateMovie(const char movie[MAX_STR], int charCounts[MAX_WORDS]);
---
+++ Ass1/main.c
@@ -0,0 +1,5 @@
+#include "gryphflix.h"
+
+int main(void) {
+    return 0;
+}
