8d13849268ddf53f342a275bcf2ed88d4b4d2e8f
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-02-12 16:35:38 -0500

    updated makefile removed header

--- lab2.0/header.h
+++ lab2.0/header.h
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 #define MAX_WORD_LENGTH 20
 #define MAX_STRING_LENGTH 100
--- lab2.0/poetry.c
+++ lab2.0/poetry.c
@@ -1,4 +1,6 @@
-#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     char currChar;
