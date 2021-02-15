d99464d0a2b52d8778721b177586726b9c1d8fcf
Eric Morse <ericm7597@gmail.com>
2021-02-14 23:55:21 -0500

    started work on readFIle, fixed makefile issue

--- Ass2/fleschIndex.c
+++ Ass2/fleschIndex.c
@@ -1,7 +1,28 @@
 #include "fleschIndex.h"
 
 char * readFile (char * filename) {
-    return 0;
+    FILE * inFile = NULL;
+    
+    inFile = fopen(filename, "r");
+
+    if (inFile == NULL) {}
+
+    char * fileString;
+    char currChar;
+    int numChar;
+
+    numChar = 0;
+    fileString = malloc(sizeof(char));
+
+    while(!feof(inFile) && inFile != NULL) {
+        fscanf(inFile, "%c", &currChar);
+        fileString[numChar] = currChar;
+
+        numChar += 1;
+        fileString = realloc(fileString, sizeof(char) * (numChar + 1));
+    }
+
+    return fileString;
 }
 
 int calculateFleschIndex(int syllables, int words, int sentences) {
--- Ass2/main.c
+++ Ass2/main.c
@@ -1,5 +1,13 @@
 #include "fleschIndex.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("ERROR | USAGE: ./fleschIndex <filename>.\n");
+        return 1;
+    }
+
+    // what is the max filename size?
+    char filename[] = argv[1];
+
     return 0;
 }
