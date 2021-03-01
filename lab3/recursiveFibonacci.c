9e6a453401636366796be6c16659580c2d37814a
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:07:51 -0500

    Implemented Recursive Fibonacci

--- lab3/recursiveFibonacci.c
+++ lab3/recursiveFibonacci.c
@@ -1,3 +1,11 @@
 #include "lab3.h"
 
-int recursiveFibonacci(int n);
+int recursiveFibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    }
+
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
