ed67c689f46b9b5a17e51ca858d9aa3ade1b04d6
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:08:05 -0500

    implemented testing for iterative and recursive fibonacci

--- lab3/main.c
+++ lab3/main.c
@@ -1,5 +1,29 @@
 #include "lab3.h"
 
 int main(void) {
+    int i;
+    int fibAnwsers[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
+    int fibAnwser;
+
+    for (i = 0; i < 15; i++) {
+        fibAnwser = iterativeFibonacci(i);
+        if (fibAnwser == fibAnwsers[i]) {
+            printf("Iterative Fibbonacci of %d: %d \033[0;32m PASS \033[0m \n", i, fibAnwser);
+        }
+        else {
+            printf("Iterative Fibbonacci of %d: %d \033[0;31m FAIL \033[0m \n", i, fibAnwser);
+        }
+    }
+
+    for (i = 0; i < 15; i++) {
+        fibAnwser = recursiveFibonacci(i);
+        if (fibAnwser == fibAnwsers[i]) {
+            printf("Recursive Fibbonacci of %d: %d \033[0;32m PASS \033[0m \n", i, fibAnwser);
+        }
+        else {
+            printf("Recursive Fibbonacci of %d: %d \033[0;31m FAIL \033[0m \n", i, fibAnwser);
+        }
+    }
+
     return 0;
 }
