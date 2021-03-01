3711544a81cb97c94b15c61df834e04ef2b6f65c
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:23:02 -0500

    Updated recursive sumNodd to account for 0

--- lab3/recursiveSumNOdd.c
+++ lab3/recursiveSumNOdd.c
@@ -3,6 +3,8 @@
 int recursiveSumNOdd(int n) {
     if (n > 1) {
         return (n * 2 - 1) + recursiveSumNOdd(n - 1);
+    } else if (n == 0) {
+        return 0;
     } else {
         return 1;
     }
