148eff9479ec47f0742f372248cd318f5ec9353d
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 12:48:39 -0500

    Implemented Iterative Fibonacci

--- lab3/iterativeFibonacci.c
+++ lab3/iterativeFibonacci.c
@@ -1,3 +1,27 @@
 #include "lab3.h"
 
-int iterativeFibonacci(int n);
+int iterativeFibonacci(int n) {
+    if (n < 0) {
+        return -1;
+    } else if (n == 0) {
+        return 0;
+    } else if (n < 3) {
+        return 1;
+    }
+
+    int i;
+    int nextTerm1;
+    int nextTerm2;
+    int currTerm;
+
+    nextTerm2 = 1;
+    currTerm = 0;
+
+    for (i = 1; i <= n; i++) {
+        nextTerm1 = nextTerm2 + currTerm;
+        currTerm = nextTerm2;
+        nextTerm2 = nextTerm1;
+    }
+
+    return currTerm;
+}
