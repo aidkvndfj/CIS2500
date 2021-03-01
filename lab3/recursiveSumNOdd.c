806d814288bf0c598620bb0583e9f4accc5853e2
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:21:47 -0500

    implemented recursive sumN odd

--- lab3/recursiveSumNOdd.c
+++ lab3/recursiveSumNOdd.c
@@ -1,3 +1,9 @@
 #include "lab3.h"
 
-int recursiveSumNOdd(int n);
+int recursiveSumNOdd(int n) {
+    if (n > 1) {
+        return (n * 2 - 1) + recursiveSumNOdd(n - 1);
+    } else {
+        return 1;
+    }
+}
