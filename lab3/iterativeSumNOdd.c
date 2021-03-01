9e8d3a0a8163d32c93cfdd3c1317479119adf7dd
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:19:12 -0500

    Implemented sumNOdd iterative

--- lab3/iterativeSumNOdd.c
+++ lab3/iterativeSumNOdd.c
@@ -1,3 +1,13 @@
 #include "lab3.h"
 
-int iterativeSumNOdd(int n);
+int iterativeSumNOdd(int n) {
+    int i;
+    int total;
+    
+    total = 0;
+    for (i = 1; i < n * 2; i += 2) {
+        total += i;
+    }
+
+    return total;
+}
