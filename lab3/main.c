b997dadf2e72aed1311870da09fe02a06d187021
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-01 13:14:15 -0500

    Implemented testing for recursive nad iterative sum N Odds

--- lab3/main.c
+++ lab3/main.c
@@ -4,6 +4,8 @@ int main(void) {
     int i;
     int fibAnwsers[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
     int fibAnwser;
+    int sumAnwsers[] = {0, 1, 4, 8, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169};
+    int sumAnwser;
 
     for (i = 0; i < 15; i++) {
         fibAnwser = iterativeFibonacci(i);
@@ -22,6 +24,26 @@ int main(void) {
         }
         else {
             printf("Recursive Fibbonacci of %d: %d \033[0;31m FAIL \033[0m \n", i, fibAnwser);
+        }
+    }
+
+    for (i = 0; i < 14; i++) {
+        sumAnwser = iterativeSumNOdd(i);
+        if (sumAnwser == sumAnwsers[i]) {
+            printf("Iterative Sum Of of %d: %d \033[0;32m PASS \033[0m \n", i, sumAnwser);
+        }
+        else {
+            printf("Iterative Sum Of of %d: %d \033[0;31m FAIL \033[0m \n", i, sumAnwser);
+        }
+    }
+
+    for (i = 0; i < 14; i++) {
+        sumAnwser = recursiveSumNOdd(i);
+        if (sumAnwser == sumAnwsers[i]) {
+            printf("Recursive Sum Of of %d: %d \033[0;32m PASS \033[0m \n", i, sumAnwser);
+        }
+        else {
+            printf("Recursive Sum Of of %d: %d \033[0;31m FAIL \033[0m \n", i, sumAnwser);
         }
     }
 
