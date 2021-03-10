59ea3f17cc88a6f4df935422b2e22c0d1daf2ed5
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-10 15:41:48 -0500

    implemented display tweets

--- Ass3/displayTweets.c
+++ Ass3/displayTweets.c
@@ -1,4 +1,21 @@
 #include "headerA3.h"
 
 void displayTweets(tweet* tweetList) {
+    tweet* tempTweet;
+    tempTweet = tweetList;
+
+    // If there is no tweets, print a error
+    if (tempTweet == NULL) {
+        printf("ERROR, NO TWEETS IN LIST\n");
+    }
+    else {
+        // loop through the tweets printing and going to the next tweet
+        while (tempTweet->next != NULL) {
+            printf("%s:%s\nid:%d\n", tempTweet->user, tempTweet->text, tempTweet->id);
+            tempTweet = tempTweet->next;
+        }
+
+        //print the final tweet
+        printf("%s:%s\nid:%d\n", tempTweet->user, tempTweet->text, tempTweet->id);
+    }
 }
