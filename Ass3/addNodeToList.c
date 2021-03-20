7775227b151ce848b64ec2eb578b7d2ae709093d
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-20 14:08:54 -0400

    Implemented addNodetoList

--- Ass3/addNodeToList.c
+++ Ass3/addNodeToList.c
@@ -2,5 +2,19 @@
 
 // linked list function - required after you create a tweet or load tweets from a file
 void addNodeToList(tweet** tweetList, tweet* node) {
-    
+    tweet * tempTweetList;
+
+    // add tweet to list
+    tempTweetList = *tweetList;
+    // if the tweet list is empty, make it first tweet
+    if (tempTweetList == NULL) {
+        *tweetList = node;
+    }
+    // otherwise loop to end of list, and append new tweet
+    else {
+        while (tempTweetList->next != NULL) {
+            tempTweetList = tempTweetList->next;
+        }
+        tempTweetList->next = node;
+    }
 }
